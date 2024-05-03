ad5c5ab215030355942eb15e831cbe9f0abe925b
Geistzzz <lilizhangchi@gmail.com>
2024-05-03 15:20:30 +0800

    daily

---
+++ utils/file_util.py
@@ -0,0 +1,20 @@
+import os
+
+
+def get_file_list(path, recursive=False):
+    """
+    获取指定路径下所有的文件名(绝对路径)
+    :param path:指定的路径
+    :param recursive:是否递归
+    :return:获取到的文件列表
+    """
+    dir_names = os.listdir(path)
+    file_list = []
+    for dir_name in dir_names:
+        absolute_path = f'{path}/{dir_name}'
+        if not os.path.isdir(absolute_path):
+            file_list.append(absolute_path)
+        else:
+            if recursive:
+                file_list += get_file_list(absolute_path, recursive=recursive)
+    return file_list
